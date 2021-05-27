c1dc964453029ab62df6efc0a9b771ae908a5e18
Santiago <santri94@gmail.com>
2021-05-27 19:56:33 -0400

    Added Main()

---
+++ CalculatorUI/src/main/java/santri94/com/calculatorui/MainClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package santri94.com.calculatorui;
+
+/**
+ *
+ * @author 12018
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Hi Santi");
+    }
+}
